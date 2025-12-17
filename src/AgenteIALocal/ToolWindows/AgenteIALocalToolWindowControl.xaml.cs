96dc41e114972cb4172a20ee4eb30757940651a4
Marco Alejandro De Santis <mdesantis@thiscloud.com.ar>
2025-12-17 21:33:23 +0100

    feat(vsix): add initial ToolWindow placeholder

---
+++ src/AgenteIALocal/ToolWindows/AgenteIALocalToolWindowControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace AgenteIALocal.ToolWindows
+{
+    /// <summary>
+    /// Code-behind for the minimal ToolWindow user control.
+    /// </summary>
+    public partial class AgenteIALocalToolWindowControl : UserControl
+    {
+        public AgenteIALocalToolWindowControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
