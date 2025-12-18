99044e04ad78397e88ee394ebe704276087c78d3
Marco Alejandro De Santis <mdesantis@thiscloud.com.ar>
2025-12-18 23:39:35 +0100

    fix(vsix): register commands by initializing OpenAgenteIALocalCommand from package InitializeAsync

--- src/AgenteIALocalVSIX/AgenteIALocalVSIXPackage.cs
+++ src/AgenteIALocalVSIX/AgenteIALocalVSIXPackage.cs
@@ -3,6 +3,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
+using AgenteIALocalVSIX.Commands;
 
 
 
@@ -49,6 +50,9 @@ namespace AgenteIALocalVSIX
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            // Initialize commands (register menu commands). OpenAgenteIALocalCommand.InitializeAsync will add commands to the OleMenuCommandService.
+            await OpenAgenteIALocalCommand.InitializeAsync(this);
         }
 
         #endregion
